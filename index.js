6a261b23a9a470158087f4e660c3a564b024fa00
Chien Hoang <chien.hoang@paysmart.com.vn>
2022-09-28 14:44:24 +0700

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 var SHA256 = require("crypto-js/sha256");
 var Base64 = require("crypto-js/enc-base64");
 
