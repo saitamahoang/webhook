8ca87176cf03237094cbf4bd8113148c8db4d988
saitamahoang <38241084+saitamahoang@users.noreply.github.com>
2023-03-11 21:48:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const mapData = new Map();
 app.get('/', (req, res) => {
   res.send('<html><meta name="zalo-platform-site-verification" content="R_AZSAYD0YbAeuyzm8aLEZ-ubXxJnqD5C30" /> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>')
 })
+
+const mapData = new Map();
+app.get('/callback', (req, res) => {
+  res.send('<html><meta name="zalo-platform-site-verification" content="R_AZSAYD0YbAeuyzm8aLEZ-ubXxJnqD5C30" /> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>')
+})
+
 app.get('/zalo_verifierR_AZSAYD0YbAeuyzm8aLEZ-ubXxJnqD5C30.html', (req, res) => {
   res.send('There Is No Limit To What You Can Accomplish Using Zalo!')
 })
